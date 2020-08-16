1d96c8917b5b60420ae8fa91a4997a816d9b4139
Mariam <mariam.elgaria98@gmail.com>
2020-08-16 16:08:49 +0300

    clac is done

--- calculator.py
+++ calculator.py
@@ -1,8 +1,23 @@
 
 def main():
-	#write your code here
-	pass
-	
+        num1=input("Enter first number: ")
+        num2=input("Enter second number: ")
+        op=time=input("Choose the operation(+, -, /, *): ")
+        listOp=["+","-","/","*"]
+        if (num1.isdigit and num2.isdigit()):
+                if(op=="+"):
+                        print("The answer is ", (int(num1)+int(num2)))
+                elif(op=="-"):
+                        print("The answer is ", (int(num1)-int(num2)))
+                elif(op=="*"):
+                        print("The answer is ", (int(num1)*int(num2)))
+                elif(op=="/"):
+                        print("The answer is ", (int(num1)/int(num2)))
+                else:
+                        print("the operation is not valid")
+        else:
+                print("the numbers are invalid")
+
 
 
 
